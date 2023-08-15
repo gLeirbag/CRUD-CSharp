2ae81a8c229a7943af57bf1914273b23d9060595
gLeirbag <gabriel.f_gomazako@outlook.com>
2023-08-15 19:20:58 -0300

    Adicionado a funcionalidade de mostrar a tabela, e renomenado as colunas.
    Amend: Melhorada a tabela.

--- CRUD CSharp/View/JanelaPrincipal.cs
+++ CRUD CSharp/View/JanelaPrincipal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CRUD_CSharp.Controller;
+using CRUD_CSharp.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+
+//Pra fazer:
+// [ ] - Mover a lógica de negócio para a camade Controller.
 
 namespace CRUD_CSharp.View
 {
@@ -46,5 +52,45 @@ namespace CRUD_CSharp.View
             
 
         }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable resultado =ProdutoDAO.Listar();
+
+                // Renomeando as colunas
+                resultado.Columns["codigo"].ColumnName =       "Código";
+                resultado.Columns["descricao"].ColumnName =    "Descrição";
+                resultado.Columns["datavalidade"].ColumnName = "Data de Validade";
+                resultado.Columns["preco"].ColumnName =        "Preço";
+                resultado.Columns["taxalucro"].ColumnName =    "Taxa de Lucro";
+
+                //Adicionando coluna do prazo de validade
+                DataColumn prazoValidade = new DataColumn("Prazo de Validade", typeof(int) );
+
+                resultado.Columns.Add(prazoValidade);
+                resultado.Columns["Prazo de Validade"].SetOrdinal(3);
+                //resultado.Columns["Prazo de Validade"].format;
+
+                //Calculando o prazo de validade.
+                foreach (DataRow row in resultado.Rows)
+                {
+                    DateTime data = (DateTime)row["Data de Validade"];
+                    row["Prazo de Validade"] = Utilidades.CalcularPrazoValidade(data).Days;
+                }
+                //resultado.Columns["Prazo de Validade"].DataType = typeof(Day);
+                
+
+                dvgTabela.DataSource = resultado;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+  
     }
 }
