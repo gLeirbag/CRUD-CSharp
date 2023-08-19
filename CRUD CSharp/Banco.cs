5d9cab0c891ec2a5383d9f8b23c7a22bd7104725
gLeirbag <gabriel.f_gomazako@outlook.com>
2023-08-18 21:34:41 -0300

    IFSP

--- CRUD CSharp/Banco.cs
+++ CRUD CSharp/Banco.cs
@@ -11,7 +11,7 @@ namespace CRUD_CSharp
     public static class Banco
     {
         // Constante de conexão com o banco de dados. Ficar de olho na senha. Isso em C# é um membro da classe categorizado como "constante".
-        private const string StringConexao = "Server=localhost;Port=5432;User Id=postgres;Password=G4briel1;Database=tocc8;";
+        private const string StringConexao = "Server=localhost;Port=5432;User Id=postgres;Password=ifsp;Database=tocc8;";
 
         //NpgsqlDataSource representa seu banco de dados PostgreSQL e pode fornecer conexões a ele ou suportar a execução direta de SQL nele.
         //NpgsqlDataSource encapsula as várias configurações do Npgsql necessárias para se conectar ao PostgreSQL, bem como o pool de conexões que torna o Npgsql eficiente.
